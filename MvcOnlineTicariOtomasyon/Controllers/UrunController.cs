2274849ea8713f64476a02c62d9016f0cf04a032
meminolgc <meminolgc@gmail.com>
2024-07-15 14:22:48 +0300

    satışlar

--- MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/UrunController.cs
@@ -74,5 +74,11 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult UrunListesi()
+        {
+            var degerler = c.Uruns.ToList();
+            return View(degerler);
+        }
     }
 }
